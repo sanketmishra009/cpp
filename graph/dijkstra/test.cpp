3d0dfe12e8b9802af77ba9eecfbf4e9339f7eab6
sanket mishra <sanketmishra@protonmail.com>
2021-01-17 15:58:58 +0530

    Dijkstra Algorithm.

--- graph/dijkstra/test.cpp
+++ graph/dijkstra/test.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class edge
 {
 public:
-    edge(int x, int val) : node(x), weight(val) {}
+    edge(int x = 25, int val = 1000) : node(x), weight(val) {}
     int node, weight;
 };
 
@@ -21,28 +22,156 @@ void showGraph(vector<edge> adj[], int size)
 {
     for (int i = 0; i < size; i++)
     {
-        cout << "adjecent list rep of vertex: " << i << "\n head";
+        cout << "adjecent list rep of vertex: " << static_cast<char>(i + 97) << "\n head";
         for (auto x : adj[i])
         {
-            cout << "->" << x.node << ", weight: " << x.weight << " ";
+            char z = 97 + x.node;
+            cout << "->" << z << ", weight: " << x.weight << " ";
         }
         cout << "\n";
     }
+}
+
+void initializeFinal(vector<edge> &final, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        edge g(i, 100);
+        final.push_back(g);
+    }
+    final.at(0).weight = 0;
+}
+
+void printVec(vector<edge> set)
+{
+    for (int i = 0; i < set.size(); i++)
+    {
+        cout << static_cast<char>(set[i].node + 97) << "\t" << set[i].weight << "\n";
+    }
+}
+edge *getMin(vector<edge> &res)
+{
+    int max = 1000;
+    edge *x = new edge();
+    for (auto i : res)
+    {
+        if (i.weight < max)
+        {
+            max = i.weight;
+            x->node = i.node;
+            x->weight = i.weight;
+        }
+    }
+    // cout << "inside min.";
+    return x;
+}
+void printEdge(edge x)
+{
+    cout << static_cast<char>(x.node + 97) << "->\t" << x.weight << "\n";
+}
+
+//function to check if a node exists in res vector or not.
+edge *checkNode(vector<edge> res, edge el)
+{
+
+    // printEdge(el);
+    edge *x1 = new edge();
+    edge *x = new edge();
+    for (int i = 0; i < res.size(); i++)
+    {
+        x = &res[i];
+        // cout << "Inside loop of checkNode" << endl;
+        // printEdge(*x);
+        // cout << "Element: ";
+        // printEdge(el);
+        if (x->node == el.node)
+        {
+            x1->node = x->node;
+            x1->weight = x->weight;
+        }
+    }
+    // cout << "X1 value: ";
+    // printEdge(*x1);
+    return x1;
+}
+
+void eraseEl(vector<edge> &res, edge x)
+{
+    for (int i = 0; i < res.size(); i++)
+    {
+        if (res[i].node == x.node)
+        {
+            res.erase(res.begin() + i);
+            res.resize(res.size());
+            break;
+        }
+    }
+}
+
+vector<edge> updateRes(vector<edge> &res, edge update)
+{
+    for (int i = 0; i < res.size(); i++)
+    {
+        edge *x = &res[i];
+        // printEdge(*x);
+        // printEdge(update);
+        if (x->node == update.node)
+        {
+            x->weight = update.weight;
+        }
+    }
+    // // cout << "updated res:" << endl;
+    // printVec(res);
+    return res;
+}
+
+void dijkstra(vector<edge> adj[], int V)
+{
+    vector<edge> res;
+    vector<edge> final;
+    initializeFinal(res, V);
+    int count = 0;
+    while (!res.empty())
+    {
+        cout << "Iteration : " << count++ << endl;
+        edge *min = NULL;
+        min = getMin(res);
+        cout << "Element to be popped: ";
+        printEdge(*min);
+        eraseEl(res, *min);
+        vector<edge> adjMin = adj[min->node];
+        edge *rem = NULL;
+        for (auto el : adjMin)
+        {
+            // cout << "Inside loop of adjMin." << endl;
+            rem = checkNode(res, el);
+            // if (rem != NULL)
+            // {
+            //     cout << "Got rem value:" << endl;
+            //     printEdge(*rem);
+            // }
+            if (rem->weight != 1000 && el.weight + min->weight < rem->weight)
+            {
+                rem->weight = el.weight + min->weight;
+                // cout << "New rem value: " << endl;
+                // printEdge(*rem);
+            }
+            //write function to update res.
+            res = updateRes(res, *rem);
+        }
+        cout << "Elements in res of size: " << res.size() << endl;
+        printVec(res);
+        final.push_back(*min);
+    }
+    // printVec(res);
+    cout << "Final distance vector." << endl;
+    printVec(final);
 }
 
 int main()
 {
     int V = 6;
     vector<edge> adj[V];
-    vector<edge> res;
-    vector<edge> sSet;
-
-    // set inf status to nodes.
-    for (int i = 0; i < V; i++)
-    {
-        edge x(i, 100);
-    }
-
     addEdge(adj, 0, 1, 5);
     addEdge(adj, 0, 2, 1);
     addEdge(adj, 1, 3, 6);
@@ -50,4 +179,8 @@ int main()
     addEdge(adj, 2, 4, 9);
     addEdge(adj, 3, 5, 3);
     addEdge(adj, 4, 5, 2);
+    // showGraph(adj, V);
+    dijkstra(adj, V);
+
+    return 0;
 }
